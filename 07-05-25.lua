5617ae71fe0c69e68ab73d75876e620500075ea4
poppyandteddy12345-source <poppyandteddy12345@gmail.com>
2026-06-12 14:34:58 -0700

    Add initial Lua script

---
+++ 07-05-25.lua
@@ -0,0 +1,11 @@
+-- Bronx Lua Script
+-- Created: 2026-06-12
+
+print("Hello from Bronx!")
+
+-- Example function
+local function sayHello(name)
+    print("Hello, " .. name .. "!")
+end
+
+sayHello("World")
